cf217309c3f74ee5e5f917da847c347af609f4bd
saniaali224 <saniaalikhan224@gmail.com>
2020-05-05 16:23:04 +0500

    lift up

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 
 // Import Route Controllers
-const  Contactform = require('./Controllers/form');
+const Contactform = require('./Controllers/form');
 
 
 
@@ -11,14 +11,16 @@ const  Contactform = require('./Controllers/form');
 // Setup Route Bindings
 exports = module.exports = function (app) {
 
-// middlewares
-// Configure app for bodyParser()
-// lets us grab data from the body of POST
+    // middlewares
+    // Configure app for bodyParser()
+    // lets us grab data from the body of POST
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(cors());
 
-   
-    app.post('/api/contactform',  Contactform.create);
-    app.post('/api/subscribe',  Contactform.Subscribe);
+    app.get("/", (req, res) => {
+        res.end("ok")
+    });
+    app.post('/api/contactform', Contactform.create);
+    app.post('/api/subscribe', Contactform.Subscribe);
 };
